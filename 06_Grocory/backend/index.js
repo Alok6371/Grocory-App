e4db9d15e2a4b0c3d9e14876fa088e3f9bfc0ade
Alok pradhan <palok2406@gmail.com>
2026-01-30 10:13:37 +0000

    update

--- 06_Grocory/backend/index.js
+++ 06_Grocory/backend/index.js
@@ -2,19 +2,23 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
+import connectDB from "./config/connectDb.js";
 
+import userRoutes from "./routes/user.route.js"
 dotenv.config();
 
+connectDB();
 const allowedOrigins = ['http://localhost:5173']
 
 
-//Middle Wares
+//Middleware
 const app = express()
 app.use(cors({
     origin: allowedOrigins, credentials: true
 }))
 app.use(cookieParser());
 
+app.use('/api/user', userRoutes)
 
 //Api Endpoint
 app.get('/', (req, res) => {
@@ -22,7 +26,6 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 4000
-
 app.listen(PORT, () => {
     console.log(`server Running on port ${PORT}`);
 
